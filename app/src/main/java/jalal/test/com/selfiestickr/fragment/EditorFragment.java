fc540927153a64a1bf909f00f26eb1a68600d463
Jalal Souky <jalalsouky@gmail.com>
2014-11-22 14:16:28 +0100

    Added image picker screen

--- app/src/main/java/jalal/test/com/selfiestickr/fragment/EditorFragment.java
+++ app/src/main/java/jalal/test/com/selfiestickr/fragment/EditorFragment.java
@@ -1,10 +1,12 @@
 package jalal.test.com.selfiestickr.fragment;
 
 import android.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import jalal.test.com.selfiestickr.R;
 import jalal.test.com.selfiestickr.view.GestureTransformationView;
@@ -14,22 +16,48 @@ import jalal.test.com.selfiestickr.view.GestureTransformationView;
  */
 public class EditorFragment extends Fragment {
 
-    public static EditorFragment newInstance() {
+    private static final String BUNDLE_IMAGE_DATA = "image_uri";
+
+    private Uri mUri;
+
+    public static EditorFragment newInstance(Uri data) {
         EditorFragment fragment = new EditorFragment();
+        Bundle args = new Bundle();
+        args.putParcelable(BUNDLE_IMAGE_DATA, data);
+        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Bundle args = getArguments();
+        if(args != null) {
+            mUri = args.getParcelable(BUNDLE_IMAGE_DATA);
+        }
+        else if(savedInstanceState != null) {
+            mUri = savedInstanceState.getParcelable(BUNDLE_IMAGE_DATA);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_editor, container, false);
-        GestureTransformationView stickerContainer = (GestureTransformationView)view.findViewById(R.id.stickerContainer);
+
+        GestureTransformationView stickerContainer = (GestureTransformationView)view.findViewById(R.id.stickerView);
         stickerContainer.setStickrDrawable(getResources().getDrawable(R.drawable.sample_stick_2));
+
+        ImageView imageView = (ImageView)view.findViewById(R.id.imageView);
+        imageView.setImageURI(mUri);
+
         return  view;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(BUNDLE_IMAGE_DATA, mUri);
     }
 }
