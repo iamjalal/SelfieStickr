258fab90943f0a69c582bef7c21875155eae2886
Jalal Souky <jalal.souky@isis-papyrus.com>
2014-11-24 18:51:07 +0100

    Application can now take pictures w/ system camera

--- app/src/main/java/jalal/test/com/selfiestickr/activity/MainActivity.java
+++ app/src/main/java/jalal/test/com/selfiestickr/activity/MainActivity.java
@@ -1,11 +1,9 @@
 package jalal.test.com.selfiestickr.activity;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 
 import jalal.test.com.selfiestickr.R;
-import jalal.test.com.selfiestickr.fragment.EditorFragment;
 import jalal.test.com.selfiestickr.fragment.ImagePickerFragment;
 
 
@@ -18,18 +16,6 @@ public class MainActivity extends Activity {
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .replace(R.id.container, ImagePickerFragment.newInstance())
-                    .commit();
-        }
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == ImagePickerFragment.RESULT_LOAD_IMAGE
-                && resultCode == Activity.RESULT_OK) {
-            getFragmentManager().beginTransaction()
-                    .replace(R.id.container, EditorFragment.newInstance(data.getData()))
                     .commit();
         }
     }
--- app/src/main/java/jalal/test/com/selfiestickr/fragment/ImagePickerFragment.java
+++ app/src/main/java/jalal/test/com/selfiestickr/fragment/ImagePickerFragment.java
@@ -1,23 +1,51 @@
 package jalal.test.com.selfiestickr.fragment;
 
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
+import android.database.Cursor;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import jalal.test.com.selfiestickr.R;
 
 
 public class ImagePickerFragment extends Fragment {
 
-    public static final int RESULT_LOAD_IMAGE = 0;
+    public static final String TAG = "SelfieStickr";
+
+    private static final String SAVED_IMAGE_URI = "savedImageUri";
+
+    public static final int RESULT_GALLERY = 100;
+    public static final int RESULT_CAMERA = 200;
+
+    private Uri mCaptureImageUri;
 
     public static ImagePickerFragment newInstance() {
         return new ImagePickerFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if(savedInstanceState != null) {
+            mCaptureImageUri = savedInstanceState.getParcelable(SAVED_IMAGE_URI);
+        }
     }
 
     @Override
@@ -26,16 +54,109 @@ public class ImagePickerFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_picker, container, false);
 
-        Button pickButtom = (Button)view.findViewById(R.id.pickButton);
-        pickButtom.setOnClickListener(new View.OnClickListener() {
+        Button galleryButton = (Button)view.findViewById(R.id.galleryButton);
+        galleryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
                 galleryIntent.setType("image/*");
-                getActivity().startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+                if (galleryIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    startActivityForResult(galleryIntent, RESULT_GALLERY);
+                }
+            }
+        });
+
+        Button cameraButton = (Button)view.findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (cameraIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException e) {
+                        Log.e(TAG, "Could not create file");
+                    }
+
+                    if (photoFile != null) {
+                        mCaptureImageUri = Uri.fromFile(photoFile);
+                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, mCaptureImageUri);
+                        startActivityForResult(cameraIntent, RESULT_CAMERA);
+                    }
+                }
             }
         });
 
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode != Activity.RESULT_OK) {
+            return;
+        }
+
+        if(requestCode == RESULT_GALLERY) {
+            startImageEditor(data.getData());
+        }
+        else if(requestCode == RESULT_CAMERA) {
+            MediaScannerConnection.scanFile(getActivity(), new String[]{mCaptureImageUri.getPath()}, null,
+                    new MediaScannerConnection.OnScanCompletedListener() {
+                        @Override
+                        public void onScanCompleted(String path, Uri uri) {
+                            Log.i("Scanned = ", "Scanned " + path);
+                            getActivity().runOnUiThread(new Runnable() {
+                                public void run() {
+                                    startImageEditor(getImageUri());
+                                }
+                            });
+                        }
+                    });
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "_" + timeStamp + "_";
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES) +
+                "/" + getResources().getString(R.string.storage_directory)+ "/");
+        if(!storageDir.exists()) {
+            storageDir.mkdirs();
+        }
+        return File.createTempFile(imageFileName, ".jpg", storageDir);
+    }
+
+    private Uri getImageUri() {
+        final String[] columns = {MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID};
+        final String orderBy = MediaStore.Images.Media.DATE_TAKEN;
+        Cursor imageCursor = getActivity().getContentResolver().query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, columns, null, null, orderBy + " DESC");
+
+        imageCursor.moveToPosition(0);
+        int dataColumnIndex = imageCursor
+                .getColumnIndex(MediaStore.Images.Media.DATA);
+        String path = imageCursor.getString(dataColumnIndex);
+        imageCursor.close();
+
+        return Uri.parse(path);
+    }
+
+    private void startImageEditor(Uri uri) {
+        getFragmentManager().beginTransaction()
+                .replace(R.id.container, EditorFragment.newInstance(uri))
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putParcelable(SAVED_IMAGE_URI, mCaptureImageUri);
     }
 }
